d5362c31a7a2e38c8323710e8c1dcbc65ef07ee1
Tomás Varas Oviedo <88008465+TomyVarasOviedo@users.noreply.github.com>
2024-11-02 22:45:02 +0000

    fix:Solucion http code 426

--- server.js
+++ server.js
@@ -2,24 +2,21 @@ const {PORT} = require("./config.js");
 const http = require('http');
 const WebSocket = require("ws");
 // Inicia el servidor WebSocket en el puerto PORT
-const server = new WebSocket.Server({ port: PORT });
 let adminClient = null
 //END POINTS DE HTTPS
-const httpServer = http.createServer((req, res) => {
+const server = http.createServer((req, res) => {
     res.writeHead(200);
     res.end("Servidor HTTP para WebSocket en Railway");
 });
-httpServer.listen(PORT, () => {
-    console.log(`Servidor WebSocket escuchando en el puerto ${PORT}`);
-});
+const socket = new WebSocket.Server({ server })
 // Manejador de eventos para cada conexión
-server.on("connection", (ws) => {
+socket.on("connection", (ws) => {
     ws.on("message", (message) => {
         const data = JSON.parse(message);
 
         // Si el total de clics es igual o mayor a 2000, enviar un mensaje de ganador
         if (data.action == "iniciar_juego") {
-            server.clients.forEach(client =>{
+            socket.clients.forEach(client =>{
                 if (client.readyState == WebSocket.OPEN) {
                     client.send(JSON.stringify({ estado:"Iniciar_juego" }))
                 }
@@ -33,4 +30,7 @@ server.on("connection", (ws) => {
             adminClient.send(JSON.stringify({winner:`${data.winner}`}))
         }
     });
+});
+server.listen(PORT, () => {
+    console.log(`Servidor WebSocket escuchando en el puerto ${PORT}`);
 });
